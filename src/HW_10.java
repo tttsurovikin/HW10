e8bbd5d2d91f8f9fd7a42cfd7f00b75dac8aaf72
tttsurovikin <stimofey03@gmail.com>
2023-10-19 14:52:03 +0200

    first commit

---
+++ src/HW_10.java
@@ -0,0 +1,14 @@
+public class HW_10 {
+    public static void main(String [] args){
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Номер шатла прямуючого до Марса номер: " + i);
+        }
+        for (int i = 5; i < 9; i++) {
+            System.out.println("Номер шатла прямуючого до Марса номер: " + i);
+        }
+        for (int i = 10; i <101 ; i++) {
+            System.out.println("Номер шатла прямуючого до Марса номер: " + i);
+        }
+    }
+}
