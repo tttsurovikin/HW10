cc90566e0d9d49e4b8e0d52177da4c3ca5c30539
tttsurovikin <stimofey03@gmail.com>
2023-10-30 12:38:24 +0100

    first commit Second commit

--- src/HW_10.java
+++ src/HW_10.java
@@ -1,14 +1,19 @@
 public class HW_10 {
-    public static void main(String [] args){
-
-        for (int i = 0; i < 4; i++) {
-            System.out.println("Номер шатла прямуючого до Марса номер: " + i);
+    public static void main(String[] args) {
+        int[] shuttleNumbers = new int[100];
+        int shuttleNumber = 1;
+        for (int i = 0; i < shuttleNumbers.length; i++) {
+            if (shuttleNumber == 4 || shuttleNumber == 9) {
+                shuttleNumber++;
+            }
+            shuttleNumbers[i] = shuttleNumber;
+            shuttleNumber++;
         }
-        for (int i = 5; i < 9; i++) {
-            System.out.println("Номер шатла прямуючого до Марса номер: " + i);
-        }
-        for (int i = 10; i <101 ; i++) {
-            System.out.println("Номер шатла прямуючого до Марса номер: " + i);
+        for (int i = 0; i < shuttleNumbers.length; i++) {
+            System.out.print(shuttleNumbers[i] + " ");
+            if ((i + 1) % 10 == 0) {
+                System.out.println();
+            }
         }
     }
 }
